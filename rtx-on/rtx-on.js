56fa44ce06bbe8050b00b92351c09ed104ba8e52
Steren <steren.giannini@gmail.com>
2023-04-20 00:50:01 +0000

    Lookup elements and extract their position

---
+++ rtx-on/rtx-on.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * @param {String} selector: CSS selector for elevated elements, defaults to children of the passed element.
+ * @param {String} element : element to apply the effect to, defaults to the entire body.
+ */
+function rtxOn(selector, element) {
+	if(!element) element = document.body;
+
+	let elements;
+	if(selector) {
+		elements = element.querySelectorAll(selector);
+	} else {
+		elements = element.children;
+	}
+
+	for (let el = 0; el < elements.length; el++) {
+		let rect = elements[el].getBoundingClientRect();
+		let x = rect.left + (rect.width / 2);
+		let y = rect.top + (rect.height / 2);
+		// ignore elements that have no height or width
+		if(rect.height === 0 || rect.width === 0) continue;
+		console.log(x, y);
+	}
+}
+
+export {rtxOn};
