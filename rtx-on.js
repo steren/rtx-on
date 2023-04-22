7d928a312cd0dc5969717fa82e640416bccfde06
Steren <steren.giannini@gmail.com>
2023-04-22 16:28:32 +0000

    import webgl-path-tracer

--- rtx-on.js
+++ rtx-on.js
@@ -1,5 +1,3 @@
-import * as THREE from 'three';
-
 /**
  * 
  * @param {String} selector: CSS selector for elevated elements, defaults to children of the passed element.
@@ -24,38 +22,17 @@ function rtxOn(selector, element) {
 		console.log(x, y);
 	}
 
-  const canvas = document.createElement('canvas');
-  element.style.background = `url(${canvas.toDataURL()})`;
-  element.style.backgroundSize = 'cover';
+  // const canvas = document.createElement('canvas');
+  // element.style.background = `url(${canvas.toDataURL()})`;
+  // element.style.backgroundSize = 'cover';
+    
+  // function animate() {
+  //   requestAnimationFrame(animate);
+  //   // Update the background with the rendered canvas
+  //   element.style.backgroundImage = `url(${canvas.toDataURL()})`;
+  // }
 
-  const renderer = new THREE.WebGLRenderer({ canvas, alpha: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setClearColor(0x000000, 0);
-  const scene = new THREE.Scene();
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.z = 5;
-
-  // TODO: replace with boxes for each elements
-  const geometry = new THREE.BoxGeometry();
-  const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-
-  function animate() {
-    requestAnimationFrame(animate);
-
-    // Update the cube's rotation
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    // Render the scene
-    renderer.render(scene, camera);
-
-    // Update the background with the rendered canvas
-    element.style.backgroundImage = `url(${canvas.toDataURL()})`;
-  }
-
-  animate();
+  // animate();
 }
 
 export {rtxOn};
